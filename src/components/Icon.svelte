31a5808d08db361fb7d0cc0ba18cd1d31dd6876d
nemo-omen <nemo.omen@gmail.com>
2023-03-29 09:54:23 -0500

    Start header

---
+++ src/components/Icon.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+import { pathsByName } from './IconPaths.js';
+
+export let name;
+export let label;
+
+const paths: string[] = pathsByName[name] || [];
+
+</script>
+
+<svg
+  aria-label={label}
+  class="c"
+  viewBox="0 0 24 24"
+  fill-rule="evenodd"
+  clip-rule="evenodd"
+>
+
+{#each paths as path}
+  <path d={path}></path>
+{/each}
+
+</svg>
+
+<style>
+  .c {
+    width: 1em;
+    height: 1em;
+    fill: currentColor;
+    transition: all 0.3s ease-out;
+    overflow: visible;
+  }
+</style>
+
+
